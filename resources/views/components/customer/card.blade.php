8da71d5ad3f62ed9157a12643572437ba93627ad
Cristiano005 <cristianosilvadesousa05@gmail.com>
2025-11-14 01:25:41 -0300

    Feat: add dynamic income badge, income icon, pagination, and customer creation modal trigger

--- resources/views/components/customer/card.blade.php
+++ resources/views/components/customer/card.blade.php
@@ -1,11 +1,30 @@
 @foreach ($customers as $customer)
     <div class="row align-items-center text-bg-dark p-3 rounded">
         <div class="col-auto">
-            <i class="bi bi-calendar-check fs-1"></i>
+            @if ($customer->income > 2500)
+                <i class="bi bi-arrow-up fs-1"></i>
+            @elseif($customer->income > 980 and $customer->income <= 2500)
+                <i class="bi bi-dash-lg fs-1"></i>
+            @else
+                <i class="bi bi-arrow-down fs-1"></i>
+            @endif
         </div>
         <div class="col-10 border-end">
-            <h6>
+            <h6 class="mb-0">
                 {{ $customer->name }}
+                @if ($customer->income <= 980)
+                    <span class="badge rounded-pill text-bg-danger">
+                        {{ $customer->income }}
+                    </span>
+                @elseif($customer->income > 980 and $customer->income <= 2500)
+                    <span class="badge rounded-pill text-bg-warning">
+                        {{ $customer->income }}
+                    </span>
+                @else
+                    <span class="badge rounded-pill text-bg-success">
+                        {{ $customer->income }}
+                    </span>
+                @endif
             </h6>
         </div>
         <div class="d-flex col-auto gap-4 ps-5">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,13 +2,22 @@
 
 @section('content')
     <section class="container mx-auto">
-        <div class="row">
-            <header class="col-12 mw-30">
+        <div class="row gap-4">
+            <header class="d-flex justify-content-between align-items-center flex-wrap col-12">
                 <h3>Customers</h3>
+                <div class="d-flex align-items-center gap-3">
+                    <button type="button" class="btn btn-outline-success">
+                        Add customer
+                        <i class="bi bi-plus-circle"></i>
+                    </button>
+                </div>
             </header>
         </div>
         <div class="row gap-3 mt-4 mx-auto">
             <x-customer.card :customers="$customers" />
+        </div>
+        <div class="mt-3">
+            {{ $customers->links() }}
         </div>
     </section>
 @endsection
