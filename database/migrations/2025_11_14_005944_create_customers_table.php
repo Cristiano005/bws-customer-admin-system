e64e4108a0cee581578556c4005e36446ccf3c3f
Cristiano005 <cristianosilvadesousa05@gmail.com>
2025-11-13 22:05:09 -0300

    feat: customer migration added

---
+++ database/migrations/2025_11_14_005944_create_customers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 150);
+            $table->string('cpf', 11)->unique();
+            $table->date('birth_date');
+            $table->date('registration_date')->default(DB::raw('CURRENT_DATE'));
+            $table->decimal('income', 10, 2)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
